d5cd46aaebbe009702774882866582b3d6d2321b
Annaliese Johnson <annajo1138@gmail.com>
2016-05-13 12:41:15 +0200

    can encrypt multiple files at once - put a space between the file names

--- CryptoFinalProject/src/come/mytest/com/EncryptTest.java
+++ CryptoFinalProject/src/come/mytest/com/EncryptTest.java
@@ -24,7 +24,7 @@ public class EncryptTest {
 	public EncryptTest(String[] args) {
 		java.util.Scanner input = new Scanner(System.in);
 
-		System.out.println("Enter file name: ");
+		System.out.println("Enter file name(s): ");
 		userFile = input.nextLine();
 		System.out.println(userFile);
 
@@ -41,67 +41,70 @@ public class EncryptTest {
 		byte[] bFile;
 		FileOutputStream out = null;
 		FileOutputStream keyfile = null;
-//		String inputFile = "input.txt";
-		String inputFile = userFile;
+		//		String inputFile = "input.txt";
+		String[] inputFiles = userFile.split(" ");
 
-		try {
-			in = new FileInputStream(inputFile);
-			out = new FileOutputStream("input_encrypted.txt");
-			inFile = new File(inputFile);
-			bFile = new byte[(int) inFile.length()];
-			keyfile = new FileOutputStream(inputFile+"_key");
 
-			SecretKey key = makeKey();
-			String keystring = Base64.getEncoder().encodeToString(key.getEncoded());
-			
-			System.out.println(keystring);
-			keyfile.write(key.getEncoded());
-			
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			
-			in.read(bFile);
-			in.close();
-			
-		    byte[] encrypted = cipher.doFinal(bFile);
-		    out.write(encrypted);
-			
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	finally {
-			if (in != null) {
+		for (String inputFile : inputFiles) {
+			try {
+				in = new FileInputStream(inputFile);
+				out = new FileOutputStream(inputFile+ "_encrypted");
+				inFile = new File(inputFile);
+				bFile = new byte[(int) inFile.length()];
+				keyfile = new FileOutputStream(inputFile+"_key");
+
+				SecretKey key = makeKey();
+				String keystring = Base64.getEncoder().encodeToString(key.getEncoded());
+
+				System.out.println(keystring);
+				keyfile.write(key.getEncoded());
+
+				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+				cipher.init(Cipher.ENCRYPT_MODE, key);
+
+				in.read(bFile);
 				in.close();
-			}
-			if (out != null) {
-				out.close();
-			}
-			if (keyfile != null) {
-				keyfile.close();
+
+				byte[] encrypted = cipher.doFinal(bFile);
+				out.write(encrypted);
+
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalBlockSizeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			finally {
+				if (in != null) {
+					in.close();
+				}
+				if (out != null) {
+					out.close();
+				}
+				if (keyfile != null) {
+					keyfile.close();
+				}
 			}
 		}
 	}
-	
+
 	private static SecretKey makeKey() throws NoSuchAlgorithmException {
 		KeyGenerator keygen = KeyGenerator.getInstance("AES");
-		
+
 		if (keygen != null) {
 			keygen.init(256);
 		}
-		
+
 		return keygen.generateKey();
 	}
 
